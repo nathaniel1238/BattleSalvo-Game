56cce8a2bbb461a817eb41c815806a3373e774e6
Nathaniel Xie <xienathaniel@gmail.com>
2023-06-13 21:54:22 -0400

    Updated Files

--- src/test/java/java/model/AIPlayerTest.java
+++ src/test/java/java/model/AIPlayerTest.java
@@ -1,11 +1,11 @@
-package java.model;
-
-import org.junit.jupiter.api.Test;
-
-class AIPlayerTest {
-
-  @Test
-  void takeShots() {
-  }
-
-}
+//package java.model;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class AIPlayerTest {
+//
+//  @Test
+//  void takeShots() {
+//  }
+//
+//}
